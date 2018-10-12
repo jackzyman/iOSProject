73438a9d771dcc7eecb6f878d0d0315996896c1f
PasitD <pasit.d@ku.th>
2018-10-12 09:26:28 +0700

    Add files via upload
    อันนี้ทำแค่หน้าUI มีหน้า HOME, CHAT, CART

---
+++ projectIOS/projectIOS/PageScanController.swift
@@ -0,0 +1,30 @@
+//
+//  PageScanController.swift
+//  projectIOS
+//
+//  Created by MosAC on 12/10/2561 BE.
+//  Copyright © 2561 MosAC. All rights reserved.
+//
+
+import UIKit
+
+class PageScanController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ projectIOS/projectIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  projectIOS
+//
+//  Created by MosAC on 11/10/2561 BE.
+//  Copyright © 2561 MosAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
